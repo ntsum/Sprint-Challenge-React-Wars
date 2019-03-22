1a3588cfca90b71c0f74694f492f4d6da8739e4f
ntsum <natsukajiwaram@gmail.com>
2019-03-22 12:16:15 -0400

    styling complete, mvp complete

--- starwars/src/components/Information.js
+++ starwars/src/components/Information.js
@@ -6,12 +6,12 @@ const Information = props => {
     return (
         <div className="Char">
             <h1 className="Name">{props.characterData.name}</h1>
-            <p className="Info"><strong>Birth Year:</strong> {props.characterData.birth_year}</p>
-            <p className="Info">Eye Color:{props.characterData.eye_color}</p>
-            <p className="Info">Gender:{props.characterData.gender}</p>
-            <p className="Info">Mass:{props.characterData.mass}</p>
-            <p className="Info">Hair-Color:{props.characterData.hair_color}</p>
-            <p className="Info">Skin-Color:{props.characterData.skin_color}</p>
+            <p className="Info"><strong>Birth Year: </strong> {props.characterData.birth_year}</p>
+            <p className="Info"><strong>Eye Color: </strong>{props.characterData.eye_color}</p>
+            <p className="Info"><strong>Gender: </strong>{props.characterData.gender}</p>
+            <p className="Info"><strong>Mass: </strong>{props.characterData.mass}</p>
+            <p className="Info"><strong>Hair-Color: </strong>{props.characterData.hair_color}</p>
+            <p className="Info"><strong>Skin-Color: </strong>{props.characterData.skin_color}</p>
         </div>
     );
 };
