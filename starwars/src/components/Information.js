c3cf0dcdef4e71a3445884b08a7c3bfd50c3d4b5
ntsum <natsukajiwaram@gmail.com>
2019-03-22 12:03:07 -0400

    mvp is complete minus styling. working on that now

--- starwars/src/components/Information.js
+++ starwars/src/components/Information.js
@@ -6,13 +6,12 @@ const Information = props => {
     return (
         <div className="Char">
             <h1 className="Name">{props.characterData.name}</h1>
-            <p className="Info">Birth Year: {props.characterData.birth_year}</p>
+            <p className="Info"><strong>Birth Year:</strong> {props.characterData.birth_year}</p>
             <p className="Info">Eye Color:{props.characterData.eye_color}</p>
             <p className="Info">Gender:{props.characterData.gender}</p>
             <p className="Info">Mass:{props.characterData.mass}</p>
             <p className="Info">Hair-Color:{props.characterData.hair_color}</p>
             <p className="Info">Skin-Color:{props.characterData.skin_color}</p>
-            <p className="Info">Films:{props.characterData.films}</p>
         </div>
     );
 };
